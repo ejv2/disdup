d708ac251effe5f5f5647a650cd26733874c1231
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-11-13 14:33:41 +0000

    Move cache to a instanced system
    Move from the dumb singleton model from before that makes it impossible
    to use multiple instances of the Duplicator.

--- cache/cache.go
+++ cache/cache.go
@@ -8,18 +8,17 @@ import (
 
 // Generic errors.
 var (
-	ErrMissing = errors.New("cache: entry not present")
-	ErrNoProvider = errors.New("cache: attempted use before setting provider")
-	ErrResetProvider = errors.New("cache: attempted to re-set provider")
+	ErrMissing     = errors.New("cache: entry not present")
+	ErrNilProvider = errors.New("cache: attempted to create cache with nil provider")
 )
 
-// Singleton package state.
-var (
+// Cache represents a cache of Discord API data objects.
+type Cache struct {
 	provider     Provider
-	channelCache = make(map[string]*discordgo.Channel)
-	userCache    = make(map[string]*discordgo.User)
-	guildCache   = make(map[string]*discordgo.Guild)
-)
+	channelCache map[string]*discordgo.Channel
+	userCache    map[string]*discordgo.User
+	guildCache   map[string]*discordgo.Guild
+}
 
 // Provider is a data provider for discord users and channels. This is mainly
 // for testing and is designed for use with either a mock or
@@ -30,35 +29,35 @@ type Provider interface {
 	Guild(guildID string) (st *discordgo.Guild, err error)
 }
 
-// SetProvider sets the provider which the cache is using for data. This must
-// be called before the cache is used and may only be called once. SetProvider
-// panics if called more than once with different providers.
-func SetProvider(p Provider) {
-	if provider != nil && provider != p {
-		panic(ErrResetProvider)
+// NewCache creates a new cache object with provider p.
+func NewCache(p Provider) *Cache {
+	if p == nil {
+		panic(ErrNilProvider)
 	}
-	provider = p
+
+	return &Cache{
+		provider:     p,
+		channelCache: make(map[string]*discordgo.Channel),
+		userCache:    make(map[string]*discordgo.User),
+		guildCache:   make(map[string]*discordgo.Guild),
+	}
 }
 
 // Channel looks up and returns a channel's data from the discord API, or
 // returns the cached value if already found. If the channel could not be
 // found, error is returned from the discord API. Errors are not cached and
 // failed lookups cause a new API hit.
-func Channel(ID string) (discordgo.Channel, error) {
-	if provider == nil {
-		panic(ErrNoProvider)
+func (c *Cache) Channel(ID string) (discordgo.Channel, error) {
+	if ch, ok := c.channelCache[ID]; ok {
+		return *ch, nil
 	}
 
-	if c, ok := channelCache[ID]; ok {
-		return *c, nil
-	}
-
-	newchan, err := provider.Channel(ID)
+	newchan, err := c.provider.Channel(ID)
 	if err != nil {
 		return discordgo.Channel{}, err
 	}
 
-	channelCache[ID] = newchan
+	c.channelCache[ID] = newchan
 	return *newchan, nil
 }
 
@@ -66,21 +65,17 @@ func Channel(ID string) (discordgo.Channel, error) {
 // cached value if already found. If the user could not be found, error is
 // returned from the discord API. Errors are not cached and failed lookups
 // cause a new API hit.
-func User(ID string) (discordgo.User, error) {
-	if provider == nil {
-		panic(ErrNoProvider)
-	}
-
-	if u, ok := userCache[ID]; ok {
+func (c *Cache) User(ID string) (discordgo.User, error) {
+	if u, ok := c.userCache[ID]; ok {
 		return *u, nil
 	}
 
-	newuser, err := provider.User(ID)
+	newuser, err := c.provider.User(ID)
 	if err != nil {
 		return discordgo.User{}, err
 	}
 
-	userCache[ID] = newuser
+	c.userCache[ID] = newuser
 	return *newuser, nil
 }
 
@@ -88,50 +83,46 @@ func User(ID string) (discordgo.User, error) {
 // the cached value if already found. If the guild could not be found, error is
 // returned from the discord API. Errors are not cached and failed lookups
 // cause a new API hit.
-func Guild(ID string) (discordgo.Guild, error) {
-	if provider == nil {
-		panic(ErrNoProvider)
-	}
-
-	if g, ok := guildCache[ID]; ok {
+func (c *Cache) Guild(ID string) (discordgo.Guild, error) {
+	if g, ok := c.guildCache[ID]; ok {
 		return *g, nil
 	}
 
-	newuser, err := provider.Guild(ID)
+	newuser, err := c.provider.Guild(ID)
 	if err != nil {
 		return discordgo.Guild{}, err
 	}
 
-	guildCache[ID] = newuser
+	c.guildCache[ID] = newuser
 	return *newuser, nil
 }
 
 // InvalidateChannel invalidates the cache entry for a given channel ID.
-func InvalidateChannel(ID string) error {
-	if _, ok := channelCache[ID]; !ok {
+func (c *Cache) InvalidateChannel(ID string) error {
+	if _, ok := c.channelCache[ID]; !ok {
 		return ErrMissing
 	}
 
-	delete(channelCache, ID)
+	delete(c.channelCache, ID)
 	return nil
 }
 
 // InvalidateUser invalidates the cache entry for a given user ID.
-func InvalidateUser(ID string) error {
-	if _, ok := userCache[ID]; !ok {
+func (c *Cache) InvalidateUser(ID string) error {
+	if _, ok := c.userCache[ID]; !ok {
 		return ErrMissing
 	}
 
-	delete(userCache, ID)
+	delete(c.userCache, ID)
 	return nil
 }
 
 // InvalidateGuild invalidates the cache entry for a given guild ID.
-func InvalidateGuild(ID string) error {
-	if _, ok := guildCache[ID]; !ok {
+func (c *Cache) InvalidateGuild(ID string) error {
+	if _, ok := c.guildCache[ID]; !ok {
 		return ErrMissing
 	}
 
-	delete(guildCache, ID)
+	delete(c.guildCache, ID)
 	return nil
 }
