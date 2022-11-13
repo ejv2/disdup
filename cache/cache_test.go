f9b7c9ddcaee013fe843140e425d9731b12b9667
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-11-13 14:42:09 +0000

    Update cache test cases

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,14 +1,9 @@
-package cache_test
+package cache
 
 import (
 	"github.com/bwmarrin/discordgo"
-	"github.com/ethanv2/disdup/cache"
 
 	"testing"
-)
-
-var (
-	panicsBeforeSetup int
 )
 
 type MockProvider struct{}
@@ -22,7 +17,7 @@ func (m MockProvider) Channel(channelID string) (c *discordgo.Channel, err error
 		}, nil
 	}
 
-	return &discordgo.Channel{}, cache.ErrMissing
+	return &discordgo.Channel{}, ErrMissing
 }
 
 func (m MockProvider) User(userID string) (u *discordgo.User, err error) {
@@ -33,7 +28,7 @@ func (m MockProvider) User(userID string) (u *discordgo.User, err error) {
 		}, nil
 	}
 
-	return &discordgo.User{}, cache.ErrMissing
+	return &discordgo.User{}, ErrMissing
 }
 
 func (m MockProvider) Guild(guildID string) (st *discordgo.Guild, err error) {
@@ -45,7 +40,7 @@ func (m MockProvider) Guild(guildID string) (st *discordgo.Guild, err error) {
 		}, nil
 	}
 
-	return &discordgo.Guild{}, cache.ErrMissing
+	return &discordgo.Guild{}, ErrMissing
 }
 
 type FakeProvider struct{}
@@ -62,68 +57,20 @@ func (m FakeProvider) Guild(guildID string) (st *discordgo.Guild, err error) {
 	return &discordgo.Guild{}, nil
 }
 
-func testPanicBeforeSetup(t *testing.T) {
-	defer func() {
-		err := recover()
-		if err == cache.ErrNoProvider {
-			panicsBeforeSetup++
-			return
-		}
-		t.Error("Unexpected panic in setup:", err)
-	}()
-
-	cache.Channel("1234")
-	cache.User("5678")
-	cache.Guild("9101112")
-
-	if panicsBeforeSetup != 3 {
-		t.Error("Expected 3 panics due to invalid setup, got", panicsBeforeSetup)
-	}
-}
-
-// testPanicOnSetProvider tests that SetProvider successfully detects an
-// invalid reassign to the provider.
-func testSetProvider(t *testing.T) {
-	panicked := false
-	prov := MockProvider{}
-
-	// Should *not* panic
-	cache.SetProvider(prov)
-	// Should *not* panic
-	cache.SetProvider(prov)
-	// *Should* panic
-	defer func() {
-		if e := recover(); e != cache.ErrResetProvider {
-			t.Error("Unexpected panic when re-setting provider to same value:", e)
-			return
-		}
-
-		panicked = true
-	}()
-	cache.SetProvider(FakeProvider{})
-
-	if !panicked {
-		t.Error("SetProvider failed to panic when setting to new value")
-	}
-}
-
-// TestSetup tests SetProvider and related invariants.
-func TestSetProvider(t *testing.T) {
-	t.Run("PanicBeforeSetProvider", testPanicBeforeSetup)
-	t.Run("SetProvider", testSetProvider)
-}
-
 func testChannel(t *testing.T) {
+	provider := MockProvider{}
+	cache := NewCache(provider)
+
 	c, err := cache.Channel("1234")
 	if err != nil {
 		t.Error("Unexpected error from channel retrieval:", err)
 	}
-	cexpect, _ := MockProvider{}.Channel("1234")
+	cexpect, _ := provider.Channel("1234")
 	if c.ID != cexpect.ID {
 		t.Error("Incorrect channel returned from retrieval")
 	}
 
-	cr, ok := (*cache.ChannelCache)["1234"]
+	cr, ok := cache.channelCache["1234"]
 	if !ok {
 		t.Error("Failed to insert channel into lookup cache")
 		return
@@ -136,25 +83,31 @@ func testChannel(t *testing.T) {
 		ID:   "testcache",
 		Name: "test channel",
 	}
-	(*cache.ChannelCache)["testcache"] = &testchan
+	cache.channelCache["testcache"] = &testchan
 	if hc, err := cache.Channel("testcache"); hc.ID != testchan.ID || err != nil {
 		t.Error("Failed to hit cache for cached channel value")
 	}
 }
 
 func testChannelError(t *testing.T) {
+	provider := MockProvider{}
+	cache := NewCache(provider)
+
 	_, err := cache.Channel("abcd")
 	if err == nil {
 		t.Error("Expected error from non-existent channel `abcd`")
 		return
 	}
 
-	if _, ok := (*cache.ChannelCache)["abcd"]; ok {
+	if _, ok := cache.channelCache["abcd"]; ok {
 		t.Error("Channel cache contains non-existent channel `abcd`")
 	}
 }
 
 func testUser(t *testing.T) {
+	provider := MockProvider{}
+	cache := NewCache(provider)
+
 	u, err := cache.User("5678")
 	if err != nil {
 		t.Error("Unexpected error from user retrieval:", err)
@@ -164,7 +117,7 @@ func testUser(t *testing.T) {
 		t.Error("Incorrect user returned from retrieval")
 	}
 
-	ur, ok := (*cache.UserCache)["5678"]
+	ur, ok := cache.userCache["5678"]
 	if !ok {
 		t.Error("Failed to insert user into lookup cache")
 		return
@@ -177,25 +130,31 @@ func testUser(t *testing.T) {
 		ID:       "testuser",
 		Username: "test user",
 	}
-	(*cache.UserCache)["testcache"] = &testuser
+	cache.userCache["testcache"] = &testuser
 	if hc, err := cache.User("testcache"); hc.ID != testuser.ID || err != nil {
 		t.Error("Failed to hit cache for cached user value")
 	}
 }
 
 func testUserError(t *testing.T) {
+	provider := MockProvider{}
+	cache := NewCache(provider)
+
 	_, err := cache.User("abcd")
 	if err == nil {
 		t.Error("Expected error from non-existent user `abcd`")
 		return
 	}
 
-	if _, ok := (*cache.UserCache)["abcd"]; ok {
+	if _, ok := cache.userCache["abcd"]; ok {
 		t.Error("Channel cache contains non-existent user `abcd`")
 	}
 }
 
 func testGuild(t *testing.T) {
+	provider := MockProvider{}
+	cache := NewCache(provider)
+
 	g, err := cache.Guild("9101112")
 	if err != nil {
 		t.Error("Unexpected error from guild retrieval:", err)
@@ -205,7 +164,7 @@ func testGuild(t *testing.T) {
 		t.Error("Incorrect guild returned from retrieval")
 	}
 
-	gr, ok := (*cache.GuildCache)["9101112"]
+	gr, ok := cache.guildCache["9101112"]
 	if !ok {
 		t.Error("Failed to insert user into lookup cache")
 		return
@@ -218,20 +177,23 @@ func testGuild(t *testing.T) {
 		ID:   "testguild",
 		Name: "test guild",
 	}
-	(*cache.GuildCache)["testcache"] = &testguild
+	cache.guildCache["testcache"] = &testguild
 	if hc, err := cache.Guild("testcache"); hc.ID != testguild.ID || err != nil {
 		t.Error("Failed to hit cache for cached guild value")
 	}
 }
 
 func testGuildError(t *testing.T) {
+	provider := MockProvider{}
+	cache := NewCache(provider)
+
 	_, err := cache.Guild("abcd")
 	if err == nil {
 		t.Error("Expected error from non-existent guild `abcd`")
 		return
 	}
 
-	if _, ok := (*cache.GuildCache)["abcd"]; ok {
+	if _, ok := cache.guildCache["abcd"]; ok {
 		t.Error("Guild cache contains non-existent user `abcd`")
 	}
 }
--- cache/export_test.go
+++
@@ -1,8 +0,0 @@
-package cache
-
-// Used to check values are being correctly inserted into the cache in tests.
-var (
-	ChannelCache = &channelCache
-	UserCache = &userCache
-	GuildCache = &guildCache
-)
