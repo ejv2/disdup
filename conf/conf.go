05dcb7fadf0ab3f83755e722ea430d94d7897e08
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-11-28 18:09:52 +0000

    Implement library-facing output config

--- conf/conf.go
+++ conf/conf.go
@@ -1,5 +1,7 @@
 package config
 
+import "github.com/ethanv2/disdup/output"
+
 // Config is the primary disdup configuration, optionally encoded in JSON
 // format and loaded by the client code. It is passed to the main duplicator,
 // which then uses it for reference.
@@ -12,6 +14,13 @@ type Config struct {
 	// configuration. This is not an optional key: servers not configured
 	// are ignored
 	Guilds map[string]GuildConfig `json:"guilds"`
+	// Outputs is a map of output names to the output interface which will
+	// be used. On duplicator startup, all outputs have their "Open" method
+	// called concurrently. On shutdown, all outputs have their "Close"
+	// method called exactly once. Outputs are not automatically decoded
+	// from json by default and must be initialized explicitly be the
+	// caller.
+	Outputs []OutputConfig `json:"-"`
 }
 
 // GuildConfig represents the configuration for a single guild. It may be
@@ -22,6 +31,8 @@ type GuildConfig struct {
 	// Disable this guild? Disabled guilds will be entirely ignored for
 	// duplication. Guilds are enabled by default
 	Disable bool `json:"disable"`
+	// Output to the output with these names. If empty, all outputs are selected.
+	Output []string `json:"output"`
 	// EnabledChannels are which channels the bot will duplicate from. This
 	// does not override "enable"; disabled guilds will still not be
 	// duplicated. If empty, all channels are enabled. Channel names should
@@ -33,4 +44,18 @@ type GuildConfig struct {
 	// users in all enabled channels will be duplicated. Users *must*
 	// include the full tag (i.e: user#tag)
 	EnabledUsers []string `json:"enabled_users"`
+}
+
+// OutputConfig represents one entry for an output handler which associates a
+// name with an output interface.
+//
+// This is not represented using a map of string to output.Output, as we need
+// to cycle through every output quite commonly, which is not as efficient
+// using a map as with a slice.
+type OutputConfig struct {
+	// Name is the name by which this output can be referenced by other
+	// configuration options.
+	Name string
+	// Output is the target for the output.
+	Output output.Output
 }
