27526b52aff0853408d589f7e27785d5a1aafbe8
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-11-29 20:34:08 +0000

    Add option parsing for print config

--- cmd/disdup/conf/output.go
+++ cmd/disdup/conf/output.go
@@ -2,6 +2,7 @@ package clconf
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -11,10 +12,37 @@ import (
 	"github.com/ethanv2/disdup/output"
 )
 
+// Output parsing or processing errors.
+var (
+	ErrWrongType      = errors.New("unexpected type")
+	ErrUnknownCollate = errors.New("unknown collation mode")
+)
+
 // An Output is a json-encodable representation of a disdup output.
 type Output struct {
 	Type      string                 `json:"type"`
 	Arguments map[string]interface{} `json:"args"`
+}
+
+func parseCollation(conf map[string]interface{}) (int, error) {
+	if rcoll, ok := conf["collate"]; ok {
+		// Unwrap option into map of options
+		coll, ok := rcoll.(string)
+		if !ok {
+			return 0, fmt.Errorf("key collate: %w", ErrWrongType)
+		}
+
+		switch coll {
+		case "channel":
+			return output.WriterCollateChannel, nil
+		case "user":
+			return output.WriterCollateUser, nil
+		default:
+			return 0, fmt.Errorf("%s: %w", coll, ErrUnknownCollate)
+		}
+	}
+
+	return 0, nil
 }
 
 // convertOutput converts a temporary representation of an output to the format
@@ -24,10 +52,25 @@ func convertOutput(name string, tmpl Output, cfg *config.Config) error {
 
 	switch tmpl.Type {
 	case "stdout":
+		col, err := parseCollation(tmpl.Arguments)
+		if err != nil {
+			return fmt.Errorf("output %s: %w", name, err)
+		}
+		rprefix, ok := tmpl.Arguments["prefix"]
+		prefix := ""
+		if ok {
+			if prefix, ok = rprefix.(string); !ok {
+				return fmt.Errorf("output %s: key prefix: %w", name, ErrWrongType)
+			}
+			prefix += " " // Append a space to properly space output in log format
+		}
 		out = &output.Writer{
 			Output:  os.Stdout,
-			Collate: 0,
+			Collate: col,
+			Prefix:  prefix,
 		}
+	default:
+		return ErrOutput
 	}
 
 	cfg.Outputs = append(cfg.Outputs, config.OutputConfig{Name: name, Output: out})
@@ -64,7 +107,7 @@ func loadOutputs(cfg *config.Config, paths []string) error {
 
 	for name, output := range outputs {
 		if err := convertOutput(name, output, cfg); err != nil {
-			return fmt.Errorf("outputs config: output %s (%s): %w", name, output.Type, ErrOutput)
+			return fmt.Errorf("outputs config: output %s (%s): %w", name, output.Type, err)
 		}
 	}
 
