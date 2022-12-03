d3ba3706947c828e891cb5cb1017ed11f2fe936f
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-12-03 21:57:38 +0000

    Add MailServer.AddrInfo test case

---
+++ output/mail_test.go
@@ -0,0 +1,48 @@
+package output_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/ethanv2/disdup/output"
+)
+
+// Test port and address parsing.
+func TestAddrParsing(t *testing.T) {
+	cases := []struct {
+		m          output.MailServer
+		ExpectErr  error
+		ExpectHost string
+		ExpectPort int
+	}{
+		{output.MailServer{Address: "abcd:1234"}, nil, "abcd", 1234},
+		{output.MailServer{Address: "abcd:-1234"}, output.ErrBadServer, "", 0},
+		{output.MailServer{Address: "abcd"}, output.ErrBadServer, "", 0},
+		{output.MailServer{Address: ":"}, output.ErrBadServer, "", 0},
+		{output.MailServer{Address: ":1234"}, output.ErrBadServer, "", 0},
+	}
+
+	for _, c := range cases {
+		gothost, gotport, err := c.m.AddrInfo()
+
+		if c.ExpectErr != nil {
+			if err == nil {
+				t.Errorf("expected error from Open() (addr: %s), got nil", c.m.Address)
+			} else if !errors.Is(err, c.ExpectErr) {
+				t.Errorf("wrong error from Open()\nexpect: %s\ngot: %s", c.ExpectErr.Error(), err.Error())
+			}
+
+			continue
+		} else if err != nil {
+			t.Errorf("unexpected error from Open() (addr: %s)", c.m.Address)
+			continue
+		}
+
+		if gothost != c.ExpectHost {
+			t.Errorf("wrong host from Open()\nexpect: %s\ngot: %s", c.ExpectHost, gothost)
+		}
+		if gotport != c.ExpectPort {
+			t.Errorf("wrong port from Open()\nexpect: %d\ngot: %d", c.ExpectPort, gotport)
+		}
+	}
+}
